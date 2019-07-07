c8a871e392a7b6e9a94943a9d4c397bf5839aa44
stu-42 <1871741606@qq.com>
2019-07-07 17:06:49 +0800

    西瓜

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var result :float_t = 10.2
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        //我要去超市
+        let igotosupermarket = true
+        //有橘子
+        let orange = true
+        
+        
+        
+        if( igotosupermarket ){
+            
+            var number = 1
+            
+            
+        
+         if( orange ){
+            
+             number  = 10
+            
+           
+        }
+            print("买 \( number ) 颗西瓜")
+        }
+    
+    func add(a: Int, b: Int) -> Int{
+        return a+b
+        
     }
-
-
 }
-
+}
